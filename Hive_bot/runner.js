ecf5514851e31223b56881963deb83199d8cc9f3
Qring33 <qring@wixnation.com>
2025-12-02 20:34:40 +0000

    Add files via upload

--- Hive_bot/runner.js
+++ Hive_bot/runner.js
@@ -1,8 +1,8 @@
 const { exec, spawn } = require('child_process');
 
 const scripts = [
-    { name: "vote", cmd: "python3", args: ["-u", "vote.py"] },      // <-- ADDED
-    { name: "blog_post", cmd: "python3", args: ["-u", "blog_post.py"] },
+    { name: "blog_post", cmd: "python3", args: ["-u", "blog_post.py"] }, // <-- moved up
+    { name: "vote", cmd: "python3", args: ["-u", "vote.py"] },
     { name: "follow", cmd: "python3", args: ["-u", "follow.py"] },
     { name: "pro_update", cmd: "python3", args: ["-u", "pro_update.py"] },
 ];
