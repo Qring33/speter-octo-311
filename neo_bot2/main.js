c089ba083ce7eef6a919e6271aaec99a8e4b9d0a
Qring33 <qring@wixnation.com>
2025-12-22 05:54:15 +0000

    Update main.js

--- neo_bot2/main.js
+++ neo_bot2/main.js
@@ -87,7 +87,7 @@ async function runSingleAccount() {
       console.log(`Session file found for ${acc.username}. Loading session...`);
 
       browser = await firefox.launchPersistentContext(profilePath, {
-        headless: false,
+        headless: true,
         userAgent: acc.user_agent,
         locale: "en-US",
         timezoneId: "America/New_York",
